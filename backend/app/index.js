2e710054ac6d66affed174e76d4de2c1345e09d7
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-04 16:54:34 +0200

    feat: add sample user route

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import userRoutes from './routes/user.ts';
 
 const app = express();
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
 
 export default app;
