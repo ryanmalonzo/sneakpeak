f50622c58b48d55911703a8de11467a53a6bd69d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-05 15:34:15 +0200

    chore: add ESLint

---
+++ backend/eslint.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+);
