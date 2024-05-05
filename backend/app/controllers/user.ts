78df895d900c272b2501ad0defa8b57e283c728a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-05 21:50:17 +0200

    feat: send activation URL using Postmark

--- backend/app/controllers/user.ts
+++ backend/app/controllers/user.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import User from '../models/user';
 import jwt from 'jsonwebtoken';
+import postmark from '../helpers/postmark';
 import { StatusCodes } from 'http-status-codes';
 
 const JWT_EXPIRY_TIME = '1h';
+const ACCOUNT_ACTIVATION_TEMPLATE_ID = 35812359;
 
 export const registerUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -15,9 +17,26 @@ export const registerUser = async (req: Request, res: Response) => {
     return res.status(StatusCodes.BAD_REQUEST).json(error);
   }
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, {
-    expiresIn: JWT_EXPIRY_TIME,
-  });
+  let token;
+  try {
+    token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: JWT_EXPIRY_TIME,
+    });
+  } catch (error) {
+    console.error('Could not sign JWT', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+
+  try {
+    // TODO: generate confirmation token and store it in db
+
+    await postmark.sendEmail(email, ACCOUNT_ACTIVATION_TEMPLATE_ID, {
+      activation_url: 'https://google.com',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
 
   return res.status(StatusCodes.CREATED).json({ token });
 };
---
+++ backend/app/helpers/postmark.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import postmark, { TemplatedMessage } from 'postmark';
+
+dotenv.config();
+
+const client = new postmark.ServerClient(
+  process.env.POSTMARK_SERVER_API_TOKEN!,
+);
+
+const sendEmail = async (
+  email: string,
+  templateId: number,
+  templateParams: object,
+) => {
+  await client.sendEmailWithTemplate(
+    new TemplatedMessage(
+      'contact@sneakpeak.store',
+      templateId,
+      templateParams,
+      email,
+    ),
+  );
+};
+
+export default {
+  sendEmail,
+};
