30df992d4279b93c14085d65853e2c71ab51ce3b
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-08 16:14:41 +0200

    feat: create email verification page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/verify-email',
       name: 'email_verification',
       component: EmailVerification
     },
