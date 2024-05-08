015de60ced2b0130b578325cf139f9075452acb3
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-05-08 22:08:03 +0200

    optimize code

--- backend/app/services/user.ts
+++ backend/app/services/user.ts
@@ -1,36 +1,23 @@
 import { HydratedDocument } from 'mongoose';
-import bcrypt from 'bcrypt';
+
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 import { StatusCodes } from 'http-status-codes';
-import { User, IUser } from '../models/user';
 import { RequestError } from '../helpers/error';
 import { PostmarkClient } from '../helpers/postmark';
+import { IUser } from '../models/user';
+import { UserManager } from './userManager';
 
-const SALT_ROUNDS = 10;
 const ACCOUNT_VERIFICATION_TEMPLATE_ID = 35812359;
 const JWT_EXPIRY_TIME = '1h';
 
 export class UserService {
-  static async findById(userId: string) {
-    return await User.findById(userId);
-  }
-
-  static async findByEmail(email: string) {
-    return await User.findOne({
-      email,
-    });
-  }
-
   static async registerUser(email: string, password: string) {
-    if (await UserService.findByEmail(email)) {
+    if (await UserManager.findByEmail(email)) {
       throw new RequestError(StatusCodes.BAD_REQUEST, 'user_already_exists');
     }
 
-    const user = new User({ email, password });
-
-    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
-    user.password = hash;
+    const user = await UserManager.create(email, password);
 
     await UserService.sendVerificationEmail(user, email);
   }
@@ -48,7 +35,7 @@ export class UserService {
     // Store token in user document
     user.challenge.email.token = emailVerificationToken;
     user.challenge.email.expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1d
-    await user.save();
+    await UserManager.update(user._id, user);
 
     await PostmarkClient.sendEmail(email, ACCOUNT_VERIFICATION_TEMPLATE_ID, {
       verification_url: `${process.env.API_URL}/api/users/${user._id}/challenge/email?token=${emailVerificationToken}`,
@@ -67,14 +54,14 @@ export class UserService {
     }
 
     user.challenge.email.verified = true;
-    await user.save();
+    await UserManager.update(user._id, user);
 
     return {
-      token: UserService.generateAuthToken(user),
+      token: UserService._generateAuthToken(user),
     };
   }
 
-  static generateAuthToken(user: HydratedDocument<IUser>) {
+  private static _generateAuthToken(user: HydratedDocument<IUser>) {
     return jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
       expiresIn: JWT_EXPIRY_TIME,
     });
