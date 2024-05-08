257ee6c30bbf99a05824e0532474acbb74248f46
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-08 16:15:32 +0200

    feat: use frontend page for email verification

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 import { UserRouter } from './routers/user';
 import { StatusCodes } from 'http-status-codes';
 import { RequestError } from './helpers/error';
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type,Authorization',
+}));
 app.use(express.json());
-app.use('/api', UserRouter);
+app.use('/', UserRouter);
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 app.use((err, req, res, next) => {
