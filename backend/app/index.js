5285aed2b45bd98dfa6461f0bed32bb78b2b592d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-19 10:03:34 +0200

    chore: allow dev deployment in cors

--- backend/app/index.js
+++ backend/app/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://dev.sneakpeak.store'],
     methods: 'GET,POST,PUT',
     allowedHeaders: 'Content-Type,Authorization',
   }),
