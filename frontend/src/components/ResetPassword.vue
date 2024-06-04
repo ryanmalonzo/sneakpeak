ddb0f0cd92d752e727e0bb6281da0853bf233956
cheick <cheicklanick@gmail.com>
2024-06-04 05:04:09 +0200

    Modification de ce fichier parce qu'avant de modifier le mdp il faut d'abord faire la demande pour recevoir un lien

--- frontend/src/components/ResetPassword.vue
+++ frontend/src/components/ResetPassword.vue
@@ -1,28 +1,17 @@
 <template>
-    <GenericModal v-model:visible="modelResetPasswordVisible" header="Réinitialiser le mot de passe">
+    <GenericModal v-model:visible="modelResetPasswordVisible" header="Mot de passe oublié">
       <form>
         <div class="flex flex-col align-items-center gap-2 mb-3">
-            <label for="password" class="w-6rem">Nouveau mot de passe</label>
-            <InputText
-                type="password"
-                id="password"
-                class="flex-auto"
-                placeholder="************"
-                v-model="password"
-            />
-        </div>
-        <div class="flex flex-col align-items-center gap-2 mb-3">
-            <label for="password1" class="w-6rem">Confirmer le mot de passe</label>
-            <InputText
-                type="password"
-                id="password1"
-                class="flex-auto"
-                placeholder="************"
-                v-model="password1"
-            />
+          <label for="email" class="w-6rem">Adresse mail</label>
+          <InputText
+            id="email"
+            class="flex-auto"
+            placeholder="amine.nairi@gmail.com"
+            v-model="email"
+          />
         </div>
         <div class="flex flex-col justify-content-end gap-2">
-          <Button type="submit" label="Réinitialiser le mot de passe" rounded></Button>
+          <Button type="submit" label="Recevoir le lien" rounded></Button>
         </div>
       </form>
     </GenericModal>
