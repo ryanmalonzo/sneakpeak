2a0fd9409b5cbdaad9a687402549364fd781e494
cheick <cheicklanick@gmail.com>
2024-06-23 00:15:34 +0200

    route pour reset le password

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import EmailVerificationView from '../views/EmailVerificationView.vue'
 import SearchView from '@/views/SearchView.vue'
 import CartView from '@/views/CartView.vue'
+import NewPassword from '@/views/ResetPasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/cart',
       name: 'cart',
       component: CartView
+    },
+    {
+      path: '/reset-password',
+      name: 'reset_password',
+      component: ResetPasswordView
     }
   ]
 })
