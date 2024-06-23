982d0df7cdc7ffe5a48e773a913887d0a7be0422
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-06-23 20:36:49 +0200

    add delete tests and update tests

--- backend/tests/unit/CartService.test.ts
+++ backend/tests/unit/CartService.test.ts
@@ -18,18 +18,17 @@ afterEach(() => {
 });
 
 describe('CartService', () => {
+  const CartProducts: CartProduct[] = [
+    {
+      cartId: 1,
+      variantId: 1,
+      quantity: 1,
+      total: 200,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    } as CartProduct,
+  ];
   describe('addProductToCart', () => {
-    const CartProducts: CartProduct[] = [
-      {
-        cartId: 1,
-        variantId: 1,
-        quantity: 1,
-        total: 200,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      } as CartProduct,
-    ];
-
     const Variant: Variant = {
       id: 1,
       sneakerId: 1,
@@ -127,6 +126,91 @@ describe('CartService', () => {
       sinon.stub(CartRepository, 'updateCart').resolves(undefined);
 
       await expect(CartService.addProductToCart(1, 1, 1)).to.be.fulfilled;
+    });
+  });
+
+  describe('updateProductInCart', () => {
+    it('should throw an error when the cart is not found', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves(null);
+      await expect(CartService.updateProductInCart(1, 1, 1)).to.be.rejectedWith(
+        'Cart not found',
+      );
+    });
+
+    it('should throw an error if the quantity is less than 1', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      sinon.stub(CartRepository, 'getCartProducts').resolves(CartProducts);
+      await expect(CartService.updateProductInCart(1, 1, 0)).to.be.rejectedWith(
+        'Quantity must be positive',
+      );
+    });
+
+    it('should throw an error if the products is not found in the cart', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      sinon.stub(CartRepository, 'getCartProducts').resolves([]);
+      await expect(CartService.updateProductInCart(1, 1, 1)).to.be.rejectedWith(
+        'Product not found',
+      );
+    });
+
+    it('should throw an error if the variant is not found', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      console.log(!CartProducts.length);
+      sinon.stub(CartRepository, 'getCartProducts').resolves(CartProducts);
+      sinon.stub(VariantRepository, 'findVariantById').resolves(null);
+      await expect(CartService.updateProductInCart(1, 1, 1)).to.be.rejectedWith(
+        'Variant not found',
+      );
+    });
+
+    it('should throw an error if the sneaker is not found', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      console.log(!CartProducts.length);
+      sinon.stub(CartRepository, 'getCartProducts').resolves(CartProducts);
+      sinon.stub(VariantRepository, 'findVariantById').resolves({} as Variant);
+      await expect(CartService.updateProductInCart(1, 1, 1)).to.be.rejectedWith(
+        'Sneaker not found',
+      );
+    });
+
+    it('should throw an error if the stock is not enough', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      console.log(!CartProducts.length);
+      sinon.stub(CartRepository, 'getCartProducts').resolves(CartProducts);
+      sinon.stub(VariantRepository, 'findVariantById').resolves({
+        stock: 5,
+      } as Variant);
+      sinon.stub(SneakerRepository, 'findSneakerById').resolves({} as Sneaker);
+      await expect(
+        CartService.updateProductInCart(1, 1, 100),
+      ).to.be.rejectedWith('Not enough stock');
+    });
+  });
+
+  describe('deleteProductFromCart', () => {
+    const CartProducts: CartProduct[] = [
+      {
+        cartId: 1,
+        variantId: 1,
+        quantity: 0,
+        total: 200,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      } as CartProduct,
+    ];
+    it('should throw an error when the cart is not found', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves(null);
+      await expect(CartService.deleteProductFromCart(1, 1)).to.be.rejectedWith(
+        'Cart not found',
+      );
+    });
+
+    it('should throw an error if the product is not found in the cart', async () => {
+      sinon.stub(CartRepository, 'getCartByUserId').resolves({} as Cart);
+      sinon.stub(CartRepository, 'getCartProducts').resolves(CartProducts);
+      await expect(CartService.deleteProductFromCart(1, 2)).to.be.rejectedWith(
+        'Product not found',
+      );
     });
   });
 });
