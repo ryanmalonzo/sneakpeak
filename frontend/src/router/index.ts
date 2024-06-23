740be02d2a1c346783751e25eed5866d7802fc6c
cheick <cheicklanick@gmail.com>
2024-06-23 05:27:05 +0200

    Page de succès pour le resetPassword

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,6 +4,7 @@ import EmailVerificationView from '../views/EmailVerificationView.vue'
 import SearchView from '@/views/SearchView.vue'
 import CartView from '@/views/CartView.vue'
 import ResetPasswordView from '@/views/ResetPasswordView.vue'
+import ResetPasswordSuccess from '@/views/ResetPasswordSuccess.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/reset-password',
       name: 'reset_password',
       component: ResetPasswordView
+    },
+    {
+      path: '/resetPasswordSuccess',
+      name: 'resetPasswordSuccess',
+      component: ResetPasswordSuccess
     }
   ]
 })
