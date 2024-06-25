1bfaa952d4963e4372a737d328d9d4b42c423821
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-06-25 13:01:28 +0200

    remove generation data with fixture and create fake data

--- backend/tests/integration/CartRouter.test.ts
+++ backend/tests/integration/CartRouter.test.ts
@@ -4,47 +4,92 @@ import request from 'supertest';
 import app from '../../app/index';
 import { before } from 'mocha';
 import { UserService } from '../../app/services/UserService';
-import { uniqueEmail } from './helpers';
 import { Challenge } from '../../app/models/sql/Challenge';
-import {
-  generateDataModelSneaker,
-  generateDataModelVariant,
-} from '../../bin/generateData';
+import { Category } from '../../app/models/sql/Category';
+import { Brand } from '../../app/models/sql/Brand';
+import { Sneaker } from '../../app/models/sql/Sneaker';
+import { faker } from '@faker-js/faker';
+import { imageUrlToBase64 } from '../../app/helpers/images';
+import { VariantRepository } from '../../app/repositories/sql/VariantRepository';
+import { Color } from '../../app/models/sql/Color';
+import { Size } from '../../app/models/sql/Size';
 
 let token = '';
 const noToken = '';
 const PASSWORD = 'DarkraiIsBest123%';
 
 before(async () => {
-  try {
-    const user = await UserService.registerUser(uniqueEmail(), PASSWORD);
+  const user = await UserService.registerUser('unique@gmail.com', PASSWORD);
 
-    const challenge = await Challenge.findOne({
-      where: { userId: user.id, type: 'email' },
+  const challenge = await Challenge.findOne({
+    where: { userId: user.id, type: 'email' },
+  });
+
+  if (!challenge) {
+    throw new Error('Challenge not found');
+  }
+
+  await UserService.verifyEmail(user, challenge.token);
+
+  const loginResponse = await request(app).post('/session').send({
+    email: user.email,
+    password: PASSWORD,
+  });
+
+  const cookie = loginResponse.headers['set-cookie'];
+  if (!cookie) {
+    throw new Error('Login failed, no cookie found');
+  }
+
+  token = cookie[0].split(';')[0]; // Prendre seulement le cookie sans les options supplémentaires
+
+  for (let i = 0; i < 3; i++) {
+    const category = await Category.create({
+      name: faker.lorem.word(),
+      slug: faker.lorem.slug(),
+      image: await imageUrlToBase64(
+        faker.image.urlLoremFlickr({ category: 'sneaker' }),
+      ),
+    });
+    const brand = await Brand.create({
+      name: faker.company.name(),
+      slug: faker.lorem.slug(),
+      image: await imageUrlToBase64(
+        faker.image.urlLoremFlickr({ category: 'logo' }),
+      ),
+    });
+    const sneaker = await Sneaker.create({
+      name: faker.commerce.productName(),
+      description: faker.lorem.sentence(),
+      price: faker.number.float({ min: 50, max: 300, multipleOf: 0.1 }),
+      categoryId: category.id,
+      brandId: brand.id,
     });
 
-    if (!challenge) {
-      throw new Error('Challenge not found');
-    }
-
-    await UserService.verifyEmail(user, challenge.token);
-
-    const loginResponse = await request(app).post('/session').send({
-      email: user.email,
-      password: PASSWORD,
+    const color = Color.build({
+      name: faker.internet.color(),
+      slug: faker.lorem.slug(),
+      hex: faker.internet.color(),
     });
 
-    const cookie = loginResponse.headers['set-cookie'];
-    if (!cookie) {
-      throw new Error('Login failed, no cookie found');
-    }
+    await color.save();
 
-    token = cookie[0].split(';')[0]; // Prendre seulement le cookie sans les options supplémentaires
-    await generateDataModelSneaker(true, 3);
-    await generateDataModelVariant(true, 3);
-  } catch (error) {
-    console.error('Setup failed:', error);
-    throw error;
+    const size = Size.build({
+      name: '42',
+      slug: '42',
+    });
+
+    await size.save();
+
+    const variant = VariantRepository.build({
+      sneakerId: sneaker.id,
+      image: await imageUrlToBase64(faker.image.urlLoremFlickr()),
+      sizeId: size.id,
+      colorId: color.id,
+      stock: 5,
+    });
+
+    await VariantRepository.save(variant);
   }
 });
 
@@ -238,106 +283,105 @@ describe('CartRouter', () => {
       await testThatInsertProductInCart();
     });
   });
-});
-
-describe('GET /cart', () => {
-  it('should return the cart', async () => {
-    await testThatGetCart();
+  describe('GET /cart', () => {
+    it('should return the cart', async () => {
+      await testThatGetCart();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should update a product in the cart', async () => {
-    await testThatUpdateProductInCart();
+  describe('PUT /cart', () => {
+    it('should update a product in the cart', async () => {
+      await testThatUpdateProductInCart();
+    });
   });
-});
 
-describe('DELETE /cart', () => {
-  it('should delete a product in the cart', async () => {
-    await testThatDeleteProductInCart();
+  describe('DELETE /cart', () => {
+    it('should delete a product in the cart', async () => {
+      await testThatDeleteProductInCart();
+    });
   });
-});
 
-describe('POST /cart', () => {
-  it('should return a 400 status code when trying to insert a product in the cart without userId', async () => {
-    await testThatCantInsertProductInCartWithoutUserId();
+  describe('POST /cart', () => {
+    it('should return a 400 status code when trying to insert a product in the cart without userId', async () => {
+      await testThatCantInsertProductInCartWithoutUserId();
+    });
   });
-});
 
-describe('POST /cart', () => {
-  it('should return a 400 status code when trying to insert a product in the cart without variantId', async () => {
-    await testThatCantInsertProductInCartWithoutProductId();
+  describe('POST /cart', () => {
+    it('should return a 400 status code when trying to insert a product in the cart without variantId', async () => {
+      await testThatCantInsertProductInCartWithoutProductId();
+    });
   });
-});
 
-describe('POST /cart', () => {
-  it('should return a 400 status code when trying to insert a product in the cart without quantity', async () => {
-    await testThatCantInsertProductInCartWithoutQuantity();
+  describe('POST /cart', () => {
+    it('should return a 400 status code when trying to insert a product in the cart without quantity', async () => {
+      await testThatCantInsertProductInCartWithoutQuantity();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should return a 400 status code when trying to update a product in the cart without userId', async () => {
-    await testThatCantUpdateProductInCartWithoutUserId();
+  describe('PUT /cart', () => {
+    it('should return a 400 status code when trying to update a product in the cart without userId', async () => {
+      await testThatCantUpdateProductInCartWithoutUserId();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should return a 400 status code when trying to update a product in the cart without variantId', async () => {
-    await testThatCantUpdateProductInCartWithoutProductId();
+  describe('PUT /cart', () => {
+    it('should return a 400 status code when trying to update a product in the cart without variantId', async () => {
+      await testThatCantUpdateProductInCartWithoutProductId();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should return a 400 status code when trying to update a product in the cart without quantity', async () => {
-    await testThatCantUpdateProductInCartWithoutQuantity();
+  describe('PUT /cart', () => {
+    it('should return a 400 status code when trying to update a product in the cart without quantity', async () => {
+      await testThatCantUpdateProductInCartWithoutQuantity();
+    });
   });
-});
 
-describe('DELETE /cart', () => {
-  it('should return a 400 status code when trying to delete a product in the cart without userId', async () => {
-    await testThatCantDeleteProductInCartWithoutUserId();
+  describe('DELETE /cart', () => {
+    it('should return a 400 status code when trying to delete a product in the cart without userId', async () => {
+      await testThatCantDeleteProductInCartWithoutUserId();
+    });
   });
-});
 
-describe('DELETE /cart', () => {
-  it('should return a 400 status code when trying to delete a product in the cart without variantId', async () => {
-    await testThatCantDeleteProductInCartWithoutProductId();
+  describe('DELETE /cart', () => {
+    it('should return a 400 status code when trying to delete a product in the cart without variantId', async () => {
+      await testThatCantDeleteProductInCartWithoutProductId();
+    });
   });
-});
 
-describe('POST /cart', () => {
-  it('should return a 404 status code when trying to insert a product in the cart with invalid userId', async () => {
-    await testThatCantInsertProductInCartWithInvalidUserId();
+  describe('POST /cart', () => {
+    it('should return a 404 status code when trying to insert a product in the cart with invalid userId', async () => {
+      await testThatCantInsertProductInCartWithInvalidUserId();
+    });
   });
-});
 
-describe('POST /cart', () => {
-  it('should return a 404 status code when trying to insert a product in the cart with invalid variantId', async () => {
-    await testThatCantInsertProductInCartWithInvalidProductId();
+  describe('POST /cart', () => {
+    it('should return a 404 status code when trying to insert a product in the cart with invalid variantId', async () => {
+      await testThatCantInsertProductInCartWithInvalidProductId();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should return a 404 status code when trying to update a product in the cart with invalid userId', async () => {
-    await testThatCantUpdateProductInCartWithInvalidUserId();
+  describe('PUT /cart', () => {
+    it('should return a 404 status code when trying to update a product in the cart with invalid userId', async () => {
+      await testThatCantUpdateProductInCartWithInvalidUserId();
+    });
   });
-});
 
-describe('PUT /cart', () => {
-  it('should return a 404 status code when trying to update a product in the cart with invalid variantId', async () => {
-    await testThatCantUpdateProductInCartWithInvalidProductId();
+  describe('PUT /cart', () => {
+    it('should return a 404 status code when trying to update a product in the cart with invalid variantId', async () => {
+      await testThatCantUpdateProductInCartWithInvalidProductId();
+    });
   });
-});
 
-describe('DELETE /cart', () => {
-  it('should return a 404 status code when trying to delete a product in the cart with invalid userId', async () => {
-    await testThatCantDeleteProductInCartWithInvalidUserId();
+  describe('DELETE /cart', () => {
+    it('should return a 404 status code when trying to delete a product in the cart with invalid userId', async () => {
+      await testThatCantDeleteProductInCartWithInvalidUserId();
+    });
   });
-});
 
-describe('DELETE /cart', () => {
-  it('should return a 404 status code when trying to delete a product in the cart with invalid variantId', async () => {
-    await testThatCantDeleteProductInCartWithInvalidProductId();
+  describe('DELETE /cart', () => {
+    it('should return a 404 status code when trying to delete a product in the cart with invalid variantId', async () => {
+      await testThatCantDeleteProductInCartWithInvalidProductId();
+    });
   });
 });
