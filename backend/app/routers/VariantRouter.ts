46a56ce56163767134db7095ac381d6287c272fc
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-07-04 19:16:06 +0200

    chore: update variants API to use new MongoDB document structure
    Variants can now be queried directly from the Sneakers router under
    /sneakers/variants. The endpoint returns a flattened version of the
    variants along with their sneaker properties.

--- backend/app/routers/VariantRouter.ts
+++ backend/app/routers/VariantRouter.ts
@@ -1,40 +1,9 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { VariantService } from '../services/VariantService';
 import { StatusCodes } from 'http-status-codes';
-import { pagination } from '../middlewares/pagination';
 import { VariantDTO } from '../models/sql/Variant';
 
 export const VariantRouter = Router();
-
-VariantRouter.get(
-  '/',
-  pagination({
-    color: 'in',
-    size: 'in',
-    price: 'range',
-    stock: 'range',
-    isBest: 'in',
-  }),
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { q, page, limit, sortOptions, filterOptions } = res.locals;
-
-      return res
-        .status(StatusCodes.OK)
-        .json(
-          await VariantService.getPaginated(
-            q,
-            page,
-            limit,
-            sortOptions,
-            filterOptions,
-          ),
-        );
-    } catch (error) {
-      next(error);
-    }
-  },
-);
 
 VariantRouter.post(
   '/',
