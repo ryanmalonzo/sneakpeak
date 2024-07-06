554666f828b0f954655c5d7a4674e837ecd67d5e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-07-06 21:06:17 +0200

    feat: add remaining migrations

---
+++ backend/migrations/11-order.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import { Migration } from '../bin/migrate';
+
+// ATTENTION à bien underscorer manuellement les noms de colonnes
+export const up: Migration = async ({
+  context: sequelize,
+}: {
+  context: Sequelize;
+}) => {
+  await sequelize.getQueryInterface().createTable('orders', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    total: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    payment_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reference: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+};
+
+export const down: Migration = async ({
+  context: sequelize,
+}: {
+  context: Sequelize;
+}) => {
+  await sequelize.getQueryInterface().dropTable('orders');
+};
