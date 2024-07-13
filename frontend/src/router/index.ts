972c1dcc1dfdd0e80ed233abb4e03356d2c5baf5
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-07-13 10:55:05 +0200

    update cart Store and add component checkout

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import EmailVerificationView from '../views/EmailVerificationView.vue'
 import SearchView from '@/views/SearchView.vue'
 import CartView from '@/views/CartView.vue'
+import CheckoutView from '@/views/CheckoutView.vue'
 import ResetPasswordView from '@/views/ResetPasswordView.vue'
 import CGUView from '@/views/legal/CGUView.vue'
 import { checkAuth } from '@/helpers/auth'
@@ -32,6 +33,11 @@ const router = createRouter({
       component: CartView
     },
     {
+      path: '/checkout',
+      name: 'checkout',
+      component: CheckoutView
+    },
+    {
       path: '/reset-password',
       name: 'reset_password',
       component: ResetPasswordView
@@ -44,7 +50,7 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: BasePageAdminView
-    },
+    }
   ]
 })
 
--- frontend/src/views/CheckoutView.vue
+++ frontend/src/views/CheckoutView.vue
@@ -2,12 +2,13 @@
 import BasePage from '@/components/BasePage.vue';
 import Checkbox from 'primevue/checkbox';
 import AutoComplete from 'primevue/autocomplete';
-import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
 import axios from 'axios';
 import { reactive, ref, watch } from 'vue';
 import { CheckoutApi } from '@/services/checkoutApi';
+import { CartStore } from '@/store/cart';
+import type { CartApi } from '@/services/cartApi';
 
 interface GeoapifyFeatureProperties {
     formatted: string
@@ -96,6 +97,13 @@ const onSubmit = async () => {
 };
 
 
+const cart: CartApi.CartApiOut = CartStore().cart;
+const cartProducts = cart.cartProduct.sort((a, b) => a.id - b.id);
+const cartTotal = cartProducts.reduce((total, product) => total + product.total, 0);
+const cartTotalItems = cartProducts.length;
+const expirationTime = new Date(cart.expiredAt);
+
+
 </script>
 
 <template>
@@ -178,25 +186,28 @@ const onSubmit = async () => {
 
 
             <div class="flex flex-col items-start gap-5 max-md:self-center"> <!-- right -->
-                <div class="flex gap-5 flex-col self-stretch items-center border border-black rounded-lg">
+                <div v-if="cartTotalItems > 0"
+                    class="flex gap-5 flex-col self-stretch items-center border border-black rounded-lg">
                     <!-- right top -->
+                    <router-link to="/checkout"
+                        class="bg-black text-white p-3 px-5 flex items-center self-stretch gap-3">
 
-                    <Button label="Commander" class="bg-black text-white p-3 px-5 flex items-center self-stretch gap-3"
-                        @click="onSubmit">
+                        <p class="flex-1 flex">Commander</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M8.38223 1.60173C8.11769 1.33718 7.68877 1.33718 7.42422 1.60173C7.15967 1.86628 7.15967 2.2952 7.42422 2.55975L11.6871 6.82267H0.677419C0.303291 6.82267 0 7.12597 0 7.50009C0 7.87422 0.303291 8.17751 0.677419 8.17751H11.6871L7.42422 12.4404C7.15967 12.705 7.15967 13.1339 7.42422 13.3985C7.68877 13.663 8.11769 13.663 8.38223 13.3985L13.8016 7.9791C14.0661 7.71455 14.0661 7.28564 13.8016 7.02109L8.38223 1.60173Z"
                                 fill="white" />
                         </svg>
-                    </Button>
 
+
+                    </router-link>
 
                     <div class="flex gap-5 flex-col items-start self-stretch p-5">
                         <p class="font-bold text-base">RÉSUMÉ DE LA COMMANDE</p>
 
                         <div class="flex items-start gap-12 self-stretch">
-                            <p class="flex flex-1"> 2 produits</p>
-                            <p class="flex flex-1">80.00 €</p>
+                            <p class="flex flex-1"> {{ cartTotalItems }} article{{ cartTotalItems > 1 ? "s" : "s" }}</p>
+                            <p class="flex flex-1">{{ cartTotal.toFixed(2) }} €</p>
 
                         </div>
                         <div class="flex items-start gap-12 self-stretch">
@@ -205,12 +216,26 @@ const onSubmit = async () => {
                         </div>
                         <div class="flex items-start gap-12 self-stretch">
                             <p class="flex flex-1 font-bold">Total</p>
-                            <p class="flex flex-1">80.00 €</p>
+                            <p class="flex flex-1">{{ cartTotal.toFixed(2) }} €</p>
                         </div>
 
                     </div>
 
 
+                </div>
+                <div v-else>
+
+                    <button type="submit" class="bg-black text-white p-3 px-5 flex items-center self-stretch gap-3">
+
+                        <RouterLink to="/search">
+                            <p class="flex-1 flex">Retour à la boutique</p>
+                        </RouterLink>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M8.38223 1.60173C8.11769 1.33718 7.68877 1.33718 7.42422 1.60173C7.15967 1.86628 7.15967 2.2952 7.42422 2.55975L11.6871 6.82267H0.677419C0.303291 6.82267 0 7.12597 0 7.50009C0 7.87422 0.303291 8.17751 0.677419 8.17751H11.6871L7.42422 12.4404C7.15967 12.705 7.15967 13.1339 7.42422 13.3985C7.68877 13.663 8.11769 13.663 8.38223 13.3985L13.8016 7.9791C14.0661 7.71455 14.0661 7.28564 13.8016 7.02109L8.38223 1.60173Z"
+                                fill="white" />
+                        </svg>
+                    </button>
                 </div>
                 <p class="font-bold">MÉTHODES DE PAIEMENT ACCEPTÉES</p>
                 <div class="flex gap-5 items-start max-md:pb-4">
@@ -219,17 +244,6 @@ const onSubmit = async () => {
                     <img src="../assets/images/cartPaiement_3.png" alt="" />
                     <img src="../assets/images/cartPaiement_4.png" alt="" />
                 </div>
-
-                <button
-                    class="md:hidden right-0 left-0 bottom-0 fixed bg-black text-white p-3 px-5 flex items-center self-stretch gap-3"
-                    @click="onSubmit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M8.38223 1.60173C8.11769 1.33718 7.68877 1.33718 7.42422 1.60173C7.15967 1.86628 7.15967 2.2952 7.42422 2.55975L11.6871 6.82267H0.677419C0.303291 6.82267 0 7.12597 0 7.50009C0 7.87422 0.303291 8.17751 0.677419 8.17751H11.6871L7.42422 12.4404C7.15967 12.705 7.15967 13.1339 7.42422 13.3985C7.68877 13.663 8.11769 13.663 8.38223 13.3985L13.8016 7.9791C14.0661 7.71455 14.0661 7.28564 13.8016 7.02109L8.38223 1.60173Z"
-                            fill="white" />
-                    </svg>
-                    Commander
-                </button>
 
             </div>
 
