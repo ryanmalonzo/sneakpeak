f848057c4a8f95744f79d51cf65834cc415a8258
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-07-20 00:42:24 +0200

    pop up cookie

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,11 @@
 import { RouterView } from 'vue-router'
 import Toast from 'primevue/toast'
 import DynamicDialog from 'primevue/dynamicdialog'
+import AppCookie from './components/AppCookie.vue'
 </script>
 
 <template>
+  <AppCookie />
   <DynamicDialog />
   <Toast />
   <RouterView />
---
+++ frontend/src/components/AppCookie.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="show"
+        class="fixed bottom-5 left-5 right-5 bg-black border border-gray-300 p-5 shadow-lg z-50 text-white">
+        <p>Ce site utilise des cookies strictement nécessaires pour son bon fonctionnement. En continuant à utiliser ce
+            site, vous acceptez l'utilisation de ces cookies.</p>
+        <button @click="closeNotice" class="mt-3 bg-white text-black px-3 py-2 rounded">Compris</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            show: true,
+        };
+    },
+    methods: {
+        closeNotice() {
+            localStorage.setItem('noticeClosed', 'true');
+            this.show = false;
+        },
+    },
+    mounted() {
+        const noticeClosed = localStorage.getItem('noticeClosed');
+        if (noticeClosed) {
+            this.show = false;
+        }
+    },
+};
+</script>
+
+<style scoped></style>
